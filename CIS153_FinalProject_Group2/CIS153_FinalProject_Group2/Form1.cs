d31f85a3cd25487dde25c43a276e3da8144d6017
eHarbaugh5 <129325140+eHarbaugh5@users.noreply.github.com>
2023-03-30 11:02:15 -0700

    Name + Inital game plan -EH
    I added my name at the top, as well as at the bottom in comments, in order to start forging some team synergy I listed some of my initial thoughts on how we may be able to progress.

--- CIS153_FinalProject_Group2/CIS153_FinalProject_Group2/Form1.cs
+++ CIS153_FinalProject_Group2/CIS153_FinalProject_Group2/Form1.cs
@@ -1,4 +1,5 @@
 ﻿//Gabriel Ball
+//Evan Harbaugh
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -17,5 +18,23 @@ namespace CIS153_FinalProject_Group2
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
+//  Quick temporary game plan:  [just my initial thoughts, thus oviously will change once we start communicating]
+//  -Classes and the forms seem pretty interchange-able for any of us to work on due to their relative simplicity
+//  -however the cpu ai would make the most sence to dedicate a single member to exclusively work on it, due to the complexity
+//   and odds that the others misinterpret/mess up the design accidentaly.
+//  -In terms of visuals I am stating now that I am awful at it, and as such, would be nice if another member who is good
+//   would vaugly take charge of that aspect [In the past I litterly just made a massive button and changed its color for the background]
+//  -Anything I said that is either completer garbage/lacking/other just say it/add/remove it because this is a group effort, and thus
+//   I am only making this temporary game plan to establish a base to go off of.
+
+//  Final personal note: I Evan Harbaugh, for "puch out coding" reference:
+//  I am most and frequently available to spend time coding thursday-sunday in general times between[11am-9pm]
+//  But am still available, just less so mon-wed.  ok im done text dumping you all
